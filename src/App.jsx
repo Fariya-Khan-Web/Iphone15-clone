75636dc35531ef1268727bd64faf06bf0685d90f
Fariya <fariya.webdev@gmail.com>
2025-03-28 00:00:13 +0600

    feat: besic project setup

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+   
+      <h1 className='text-6xl text-center text-gray-400 font-semibold my-40'>IPhone15</h1>
+      
+    </>
+  )
+}
+
+export default App
