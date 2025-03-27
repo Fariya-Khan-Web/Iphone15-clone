c368979a33a4a69f853df8ec09857a79ffef4218
Fariya <fariya.webdev@gmail.com>
2025-03-28 05:37:07 +0600

    feat: project setup

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,20 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  theme: {
+    extend: {
+      colors: {
+        blue: "#2997FF",
+        gray: {
+          DEFAULT: "#86868b",
+          100: "#94928d",
+          200: "#afafaf",
+          300: "#42424570",
+        },
+        zinc: "#101010",
+      },
+    },
+  },
   plugins: [
     tailwindcss(),
     react()
